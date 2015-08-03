1262fde6ca5bc5275dfcf714f3e3e69b617a0d6c
HALFPOTATO <alexandertmai@gmail.com>
2015-08-02 21:00:52 -0700

    Added isometric projection functions.

--- main.lua
+++ main.lua
@@ -1,13 +1,33 @@
 debug = true
+require 'util/Tileset'
+require 'util/Tile'
+
+o = {tsetname = "assets/Tileset.png", twidth = 128, theight = 128}
+globaltime = 0
 
 function love.load(arg)
-
+	o = Tileset.new(o)
+	p = love.graphics.newImage("assets/Tileset.png")
 end
 
 function love.update(dt)
-
+	globaltime = dt + globaltime
 end
 
 function love.draw()
 	love.graphics.print("Hello World", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
+	-- love.graphics.draw(o.tset, o[2], 100, 100)
+	angle = (180 * math.sin(globaltime / 2) + 180)
+	pts = rotatedIsoPoints(angle, o.twidth, o.theight)
+	np = {5, 2, 4, 9, 10, 20}
+	--[[
+	np2 = { pts.top.x, pts.top.y, 
+			pts.right.x, pts.right.y,
+			pts.bot.x, pts.bot.y, 
+			pts.left.x, pts.left.y}
+	np2 = vadd(100, np2)
+	]]
+	pts = vadd(100, pts)
+	print(angle)
+	love.graphics.polygon('fill', pts)
 end
--- util/Tile.lua
+++ util/Tile.lua
@@ -7,4 +7,94 @@ function Tile:new (objdata)
 	return objdata
 end
 
+function toIso(x, y)
+	x, y = (2 * y + x) / 2, (2 * y - x) / 2
+	return x, y
+end
 
+function toCaut(x, y)
+	local a = (x - y) / 2
+	return a, (a + y) / 2
+end
+
+function vadd(n, ...)
+	if type(...) == 'table' then
+		local t = ...
+
+		for i, line in ipairs(t) do
+			t[i] = line + n
+		end
+		return t
+	else
+		print("Function v-add requires a table as an input.")
+		return nil
+	end
+end
+
+-- Return points of a tile in isometric view as if the center of the tile is the position
+function rotatedIsoPoints(degree, tWidth, tHeight)
+	-- Rotate around center of tile
+	center = {x = tWidth / 2, y = tHeight / 2}
+
+	conversion = 2 * math.pi / 360
+
+	deg = degree + 45
+
+	-- Angles for each of the points + rotation
+	a = {}
+	a.top = 3 * math.pi / 4 	+ conversion * deg
+	a.right = math.pi / 4		+ conversion * deg
+	a.bot = 5 * math.pi / 4 	+ conversion * deg
+	a.left = 7 * math.pi / 4	+ conversion * deg
+
+	p = {}
+
+	-- Convert angles to points 
+	--[[
+	p.top = {x = math.cos(a.top) * tWidth / 2, y = math.sin(a.top) * tHeight / 2}
+	p.right = {x = math.cos(a.right) * tWidth / 2, y = math.sin(a.right) * tHeight / 2}
+	p.bot = {x = math.cos(a.bot) * tWidth / 2, y = math.sin(a.bot) * tHeight / 2}
+	p.left = {x = math.cos(a.left) * tWidth / 2, y = math.sin(a.left) * tHeight / 2}
+	]]
+	p = {math.cos(a.top) * tWidth / 2, math.sin(a.top) * tHeight / 2,
+		 math.cos(a.right) * tWidth / 2, math.sin(a.right) * tHeight / 2,
+		 math.cos(a.left) * tWidth / 2, math.sin(a.left) * tHeight / 2,
+	 	 math.cos(a.bot) * tWidth / 2, math.sin(a.bot) * tHeight / 2}
+	
+	--[[
+	p.top.x, p.top.y = toIso(p.top.x, p.top.y)
+	p.right.x, p.right.y = toIso(p.right.x, p.right.y)
+	p.bot.x, p.bot.y = toIso(p.bot.x, p.bot.y)
+	p.left.x, p.left.y = toIso(p.left.x, p.left.y)
+	]]
+	p[1], p[2] = toIso(p[1], p[2])
+	p[3], p[4] = toIso(p[3], p[4])
+	p[5], p[6] = toIso(p[5], p[6])
+	p[7], p[8] = toIso(p[7], p[8])
+
+	-- Rotate it right because love2d uses some stupid topleft origin
+	angle = math.pi * 3 / 4
+	for i = 1, 8, 2 do
+		p[i], p[i + 1] = rotate(angle, p[i], p[i + 1])
+	end
+	return p
+end
+
+function rotate(deg, x, y)
+	return (math.cos(deg) * x) + (math.sin(deg) * y * -1), math.sin(deg) * x + math.cos(deg) * y
+end
+
+function strtable(t)
+	local constr = "Table: \n"
+	
+	for i, line in next, t, nil do
+		if type(line) == 'table' then
+			constr = constr .. strtable(line)
+		else
+			constr = constr .. tostring(i) .. ": " .. tostring(line)
+		end
+		constr = constr .. "\n"
+	end
+	
+	return constr
+end
