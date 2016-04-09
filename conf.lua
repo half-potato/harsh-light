b1d0f307ee145afd527f6e0828e1280f5114d307
Alexander Mai <amai@Alexanders-MacBook-Pro.local>
2016-04-08 20:44:53 -0700

    Restored items

---
+++ conf.lua
@@ -0,0 +1,10 @@
+ Configuration
+
+function love.conf(t)
+       t.title = "Harsh Light - Alpha .0001"
+       t.version = "0.10.0"
+       t.window.width = 800
+       t.window.height = 800
+
+       t.console = true
+end
